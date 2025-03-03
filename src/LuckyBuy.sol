40a4d8fb260761bc5ff11e498a6a266840cecee9
tenthirtyone <alex.sherbuck@magiceden.io>
2025-03-03 14:49:11 -0600

    add receivable and balance

--- src/LuckyBuy.sol
+++ src/LuckyBuy.sol
@@ -1,4 +1,14 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract LuckyBuy {}
+contract LuckyBuy {
+    uint256 public balance;
+
+    function _depositTreasury(uint256 amount) internal {
+        balance += amount;
+    }
+
+    receive() external payable {
+        _depositTreasury(msg.value);
+    }
+}
