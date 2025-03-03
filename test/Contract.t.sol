4f5cda32d971c15e25dba8718bdd487f5487a4bb
tenthirtyone <alex.sherbuck@magiceden.io>
2025-03-03 14:35:57 -0600

    udpate from template to boilerplate

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -3,21 +3,12 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 
-import "src/Contract.sol";
+import "src/LuckyBuy.sol";
 
-contract TestContract is Test {
-    Contract c;
+contract TestLuckyBuy is Test {
+    LuckyBuy luckyBuy;
 
     function setUp() public {
-        c = new Contract();
-    }
-
-    function testBar() public {
-        assertEq(uint256(1), uint256(1), "ok");
-    }
-
-    function testFoo(uint256 x) public {
-        vm.assume(x < type(uint128).max);
-        assertEq(x + x, x * 2);
+        luckyBuy = new LuckyBuy();
     }
 }
